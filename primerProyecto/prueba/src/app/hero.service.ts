394ad1d0a1159a5fda9918a5c8b42d736f8d41d8
kratossore069 <victorgabrielarostegui@gmail.com>
2022-07-12 00:33:43 +0100

    Mensajes terminado

--- primerProyecto/prueba/src/app/hero.service.ts
+++ primerProyecto/prueba/src/app/hero.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HeroInterfaz } from './interfaces/heroe';
 import { HEROESLISTA } from './mocks/mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessageServiceService } from './message-service.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService:MessageServiceService) { }
 
   /**
    * FUNCION QUE DEVUELVE EL MOCK DE HEROES
@@ -16,6 +17,7 @@ export class HeroService {
    */
   getHeroes(): Observable<HeroInterfaz[]> {
     const heroes = of(HEROESLISTA);
+    this.messageService.add("Lista de héroes mostrada")
     return heroes;
   }
 }
--- primerProyecto/prueba/src/app/heroes/heroes.component.ts
+++ primerProyecto/prueba/src/app/heroes/heroes.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { HeroInterfaz } from "../interfaces/heroe";
+import { HeroInterfaz } from '../interfaces/heroe';
 import { HeroService } from '../hero.service';
+import { MessageServiceService } from '../message-service.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-
   heroes: HeroInterfaz[] = [];
   selectedHero?: HeroInterfaz;
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService,
+    private messageService:MessageServiceService) {}
 
   ngOnInit(): void {
     this.getHeroes();
@@ -20,10 +21,10 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: HeroInterfaz): void {
     this.selectedHero = hero;
+    this.messageService.add("Héroe seleccionado: "+hero.nombre);
   }
 
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
-
 }
