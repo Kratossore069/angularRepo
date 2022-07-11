aed80767c7f328256ed1b8cbd0112c90fabd4c66
Kratossore069 <victorgabrielarostegui@gmail.com>
2022-07-11 14:41:08 +0100

    Avances con tour de heroes

---
+++ primerProyecto/prueba/src/app/mocks/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from '../interfaces/heroe';
+
+export const HEROES: Hero[] = [
+    { id: 12, nombre: 'Batman' },
+    { id: 13, nombre: 'Superman' },
+    { id: 14, nombre: 'Celeritas' },
+    { id: 15, nombre: 'Flash' },
+    { id: 16, nombre: 'RubberMan' },
+    { id: 17, nombre: 'Robin' },
+    { id: 18, nombre: 'Dr. IQ' },
+    { id: 19, nombre: 'Magneto' },
+    { id: 20, nombre: 'Tornado' }
+];
